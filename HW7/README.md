7cffd51d5ed2e7d7be404cb6b551c971eacc1e0e
JakeCardillo <jcardillo@outlook.com>
2020-11-24 13:41:45 -0500

    HW7 added

---
+++ HW7/README.md
@@ -0,0 +1,11 @@
+Jake Cardillo
+Homework 6 - GUI Programming I
+UMASS Lowell Comp Sci
+jacob_cardillo@student.uml.edu
+
+My page still works as required but I was unable to get my validation to run. I have compared it to several examples
+and error checked it as much as I can but I could not figure out why it is not running. As it stands, all errors are still 
+resolved using built in HTML form tools and JavaScript, except for a limit on large numbers.
+
+Site is available here: https://jakecardillo.github.io/Homepage/HW6/mult_table.html
+Repository: https://github.com/JakeCardillo/Homepage/tree/gh-pages/HW6
---
+++ HW7/js/table.js
@@ -0,0 +1,164 @@
+/*
+Jake Cardillo
+Homework 7 - GUI Programming I
+UMASS Lowell Comp Sci
+jacob_cardillo@student.uml.edu
+
+This file contains the function that creates the multiplication table and
+validates the form, as well as updating the sliders
+*/
+
+
+//Validation
+$(document).ready(function() {
+  $("#myForm").validate({
+    rules: { //all inputs required, no numbers over 4 digits
+      minX: {
+        required: true,
+        maxlength: 3
+      },
+      maxX: {
+        required: true,
+        maxlength: 3
+      },
+      minY: {
+        required: true,
+        maxlength: 3
+      },
+      maxY: {
+        required: true,
+        maxlength: 3
+      }
+    },
+    messages: {
+      minX: {
+        required: "Please enter a minimum value for x",
+        maxlength: "Please pick a number less than 4 digits"
+      },
+      maxX: {
+        required: "Please enter a maximum value for x",
+        maxlength: "Please pick a number less than 4 digits"
+      },
+      minY: {
+        required: "Please enter a minimum value for y",
+        maxlength: "Please pick a number less than 4 digits"
+      },
+      maxY: {
+        required: "Please enter a maximum value for y",
+        maxlength: "Please pick a number less than 4 digits"
+      },
+    }, //I cannot get the submitHandler to work, the validation never runs
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
+
+//This function makes sure that the sliders update when numbers are entered in the text boxes
+function update(num) {
+  var slider, loc;
+
+  //loc and slider are adjusted to get correct location
+  switch (num) {
+    case 1:
+      loc = "minX";
+      slider = "slider1";
+      break;
+    case 2:
+      loc = "maxX";
+      slider = "slider2";
+      break;
+    case 3:
+      loc = "minY";
+      slider = "slider3";
+      break;
+    case 4:
+      loc = "maxY";
+      slider = "slider4";
+      break;
+  }
+
+  var value = document.forms["myForm"][loc].value;
+  $(slider).slider("value", value);
+}
+
+function createTable() {
+  //get numbers
+  var minX = document.forms["minMax"]["minX"].value;
+  var maxX = document.forms["minMax"]["maxX"].value;
+  var minY = document.forms["minMax"]["minY"].value;
+  var maxY = document.forms["minMax"]["maxY"].value;
+
+  var errors = document.getElementById("errors");
+
+  if (minX > maxX)
+  {
+    var temp = minX;
+    minX = maxX;
+    maxX = temp;
+    errors.innerHTML = "Warning! Min value larger than max value. Values have been switched";
+  }
+
+  if (minY > maxY)
+  {
+    var temp = minY;
+    minY = maxY;
+    maxY = temp;
+    errors.innerHTML = "Warning! Min value larger than max value. Values have been switched";
+  }
+
+  //remove the old table if it exists
+  var oldTable = document.getElementById('myTable');
+  if(oldTable != null){
+    oldTable.parentNode.removeChild(oldTable);
+  }
+
+  //create the new table
+  var table = document.createElement('table')
+  table.id = "myTable"
+
+  //create the sides
+  var thead = table.createTHead();
+  var row = thead.insertRow();
+
+  //0 origin
+  var th = document.createElement('th');
+  var text = document.createTextNode("x");
+  th.appendChild(text);
+  row.appendChild(th);
+
+  //headings for x axis
+  for (var i = minX; i <= maxX; i++)
+  {
+    var th = document.createElement('th');
+    var text = document.createTextNode(i);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+
+  //fill out rows
+  for (var i = minY; i <= maxY; i++)
+  {
+    //create a row
+    var tr = document.createElement('tr');
+
+    //create the header for that row
+    var th = document.createElement('th');
+    var text = document.createTextNode(i);
+    th.appendChild(text);
+    tr.appendChild(th);
+
+    //fill out the cells in each row
+    for (var j = minX; j <= maxX; j++)
+    {
+      var product = i * j;
+
+      var td = document.createElement('td');
+      var text = document.createTextNode(product);
+      td.appendChild(text);
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  document.body.appendChild(table);
+}
